13cb27f6374f9a2370fe5c31cce44b98d3af709b
AndriyChmykhal <chmykhal@ukr.net>
2020-05-20 22:08:31 +0300

    selenium homework

--- src/test/java/com/example/selenium/LoginTest.java
+++ src/test/java/com/example/selenium/LoginTest.java
@@ -1,11 +1,13 @@
 package com.example.selenium;
 
+import com.example.selenium.steps.UserSteps;
 import org.testng.annotations.Test;
 
 public class LoginTest extends BaseTest {
 
     @Test
     public void login() {
-        // TODO use steps for the login and logout
+        userSteps.login("tomsmith", "SuperSecretPassword!")
+                .logout();
     }
 }
